5764803ed0ac6ecebed42bc3f9e73deb6d1dc197
e-tuchs <e.tuchs@gmail.com>
2017-03-25 06:37:22 -0400

    add setup.py for repo

---
+++ core/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+setup(
+    name='origin-core',
+    version='1.0',
+    packages=find_packages(),
+    license='',
+    long_description=open('README.txt').read(),
+    author='e-tuchs',
+    author_email='xuwulin0@gmail.com',
+    maintainer='estore',
+    url='http://www.lapduck.com/',
+    package_data = {
+            "origincore": ['locale/zh_CN/LC_MESSAGES/*.mo'],
+        }
+)
+
